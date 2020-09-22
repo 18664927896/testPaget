fc6fcc2f376014011ab2d873fd8519bf01c16f6c
尚钢 <>
2020-09-22 14:11:09 +0800

    一个测试包

---
+++ Example/Pods/Target Support Files/Pods-testPaget_Example/Pods-testPaget_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testPaget_Example : NSObject
+@end
+@implementation PodsDummy_Pods_testPaget_Example
+@end
---
+++ Example/Pods/Target Support Files/testPaget/testPaget-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_testPaget : NSObject
+@end
+@implementation PodsDummy_testPaget
+@end
